e610f1287f854b2a50597dc8ec0f2749de5da563
Matt Watkajtys <watkajtys@gmail.com>
2013-03-18 10:37:17 -0400

    ARGV Checks

--- router.rb
+++ router.rb
@@ -4,4 +4,13 @@ if ARGV.empty?
 	puts "USAGE: bla"
 else
 	puts "You passed in arguements! YAY!"
+
+	if ARGV[0] == "index"
+		puts "Action: INDEX"
+	elsif ARGV[0] == "show"
+		puts "Action: Show"
+	else
+		puts "Refer to USAGE. 'index' or 'show' are acceptable."
+	end
+		
 end
