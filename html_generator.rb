f0e40a90aa971bc10c8bfa6770ceb57d18d69983
Matt Watkajtys <watkajtys@gmail.com>
2013-03-18 11:04:09 -0400

    Added separate methods to generate header and footer. DRYing the code out to get rid of repeated lines.

--- html_generator.rb
+++ html_generator.rb
@@ -1,9 +1,27 @@
 class HtmlGenerator
 	def show 
-		puts "Action: Show"
+		print_header
+		puts "Action Show"
+		print_footer
+		
 	end
 
 	def index
+		print_header
 		puts "Action: Index"
+		print_footer
+	end
+
+	def print_header
+		puts "<html>"
+		puts 	"<head>"
+		puts 		"<title>Connoisseur in the head</title>"
+		puts 	"</head>"
+		puts 	"<body>"
+	end
+
+	def print_footer
+		puts 	"</body>"
+		puts "</html>"
 	end
 end
